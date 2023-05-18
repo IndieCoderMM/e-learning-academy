e83529945602754c609d9ecfae81b0b664aa3248
tobuya <thomasobuya95@gmail.com>
2023-05-18 17:05:30 +0300

    Create app components with routes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import Reservation from '../pages/Reservation';
+import NewCourse from '../pages/NewCourse';
+import DeleteCourse from '../pages/DeleteCourse';
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new_course" element={<NewCourse />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/delete_course" element={<DeleteCourse />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ app/javascript/pages/Reservation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Reservation() {
+  return (
+    <div>Reservation</div>
+  );
+}
+
+export default Reservation;
