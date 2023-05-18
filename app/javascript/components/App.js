46cff70c388f2872ad26bf9e95995c1209c4954e
heinthant <hthant00chk@gmail.com>
2023-05-18 21:04:30 +0630

    Add navlinks in sidebar

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Reservation from '../pages/Reservation';
 import NewCourse from '../pages/NewCourse';
 import DeleteCourse from '../pages/DeleteCourse';
+import SideNavbar from './SideNavbar';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new_course" element={<NewCourse />} />
-        <Route path="/reservation" element={<Reservation />} />
-        <Route path="/delete_course" element={<DeleteCourse />} />
-      </Routes>
-    </Router>
+    <main>
+      <SideNavbar />
+      <section>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new_course" element={<NewCourse />} />
+          <Route path="/reservation" element={<Reservation />} />
+          <Route path="/delete_course" element={<DeleteCourse />} />
+        </Routes>
+      </section>
+    </main>
   );
 }
 
