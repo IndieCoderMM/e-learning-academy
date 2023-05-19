52b1e2ddebe928a2f07ac1264f14b54f517c33c7
tobuya <thomasobuya95@gmail.com>
2023-05-19 23:49:49 +0300

    Retrive courses data from the redux store and display them

--- app/javascript/pages/Home.js
+++ app/javascript/pages/Home.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { coursesActions } from '../store/coursesSlice';
 
-function Home() {
+const Home = () => {
+  const dispatch = useDispatch();
+  const courses = useSelector((state) => state.courses.courses);
+  const loading = useSelector((state) => state.courses.loading);
+
+  useEffect(() => {
+    dispatch(coursesActions.fetchCourses());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>All Courses</h1>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <h2>{course.title}</h2>
+          <p>{course.description}</p>
+          <p>
+            Price: $
+            {course.price}
+          </p>
+          <p>
+            Duration:
+            {course.duration}
+            {' '}
+            minutes
+          </p>
+          <img src={course.img_url} alt={course.title} />
+          <p>
+            Instructor:
+            {course.instructor}
+          </p>
+        </div>
+      ))}
+    </div>
   );
-}
+};
 
 export default Home;
