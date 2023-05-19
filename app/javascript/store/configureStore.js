c96e20e35494f557a278865e292441ad48418ba1
tobuya <thomasobuya95@gmail.com>
2023-05-19 23:47:55 +0300

    Import coursesReducer and configure the redux store

--- app/javascript/store/configureStore.js
+++ app/javascript/store/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user';
+import coursesReducer from './coursesSlice';
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    courses: coursesReducer,
   },
 });
