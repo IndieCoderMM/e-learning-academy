b25345e3c3425b759b548ebe8eabc85ac2db32b7
tobuya <thomasobuya95@gmail.com>
2023-05-19 21:09:06 +0300

    Define an API endpoint that retrieves a list of all courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'root#index'
-  
+
   post '/users/login', to: 'users#login'
   post '/users/register', to: 'users#create'
+
+  namespace :api do
+    resources :courses
+  end
 end
