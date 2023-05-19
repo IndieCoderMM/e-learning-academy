f6d40c8998f37ceb9ef57e102ca3ef861ab3ff83
tobuya <thomasobuya95@gmail.com>
2023-05-19 21:44:08 +0300

    Add some sample data in the courses table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Course.create(
+  title: "Programming with Python",
+  description: "Learn the basics of programming in python.",
+  price: 399,
+  duration: 30,
+  img_url: "https://example.com/intro-programming.jpg",
+  instructor: "Thomas Obuya"
+)
+
+Course.create(
+  title: "Web Development Fundamentals",
+  description: "Build foundational skills in web development.",
+  price: 149,
+  duration: 45,
+  img_url: "https://example.com/web-dev-fundamentals.jpg",
+  instructor: "Hein Thant"
+)
+
+Course.create(
+  title: "Ruby on Rails full course",
+  description: "Build foundational skills in Ruby on Rails.",
+  price: 109,
+  duration: 40,
+  img_url: "https://example.com/web-dev-fundamentals.jpg",
+  instructor: "Tanveer Ahmad"
+)
+
+Course.create(
+  title: "React and Redux full course",
+  description: "Build foundational skills in React and Redux.",
+  price: 169,
+  duration: 55,
+  img_url: "https://example.com/web-dev-fundamentals.jpg",
+  instructor: "Peter Ugwu"
+)
+
