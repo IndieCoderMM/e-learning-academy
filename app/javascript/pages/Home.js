f8801367d5a58d058943c190195834000001645c
tobuya <thomasobuya95@gmail.com>
2023-05-23 12:44:34 +0300

    Solve linters

--- app/javascript/pages/Home.js
+++ app/javascript/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { coursesActions } from '../store/coursesSlice';
 import CourseItem from '../components/CourseItem';
