0417f730aca4ac7d42fbd1d4c3e2afd79e7cefc8
tobuya <thomasobuya95@gmail.com>
2023-05-23 18:13:17 +0300

    Create an action for creating a new course

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -3,4 +3,19 @@ class Api::CoursesController < ApplicationController
     @courses = Course.all
     render json: @courses, status: :ok
   end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      render json: @course, status: :created
+    else
+      render json: { errors: @course.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description, :price, :duration, :instructor)
+  end
 end
