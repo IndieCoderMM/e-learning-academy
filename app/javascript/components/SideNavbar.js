0a4938b826cc87cf82ce80a18d3feb6caf940072
heinthant <hthant00chk@gmail.com>
2023-05-24 13:38:42 +0630

    Create sidebar UI

--- app/javascript/components/SideNavbar.js
+++ app/javascript/components/SideNavbar.js
@@ -1,23 +1,65 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { FaFacebookF, FaTwitter } from 'react-icons/fa';
+import { TfiGithub, TfiLinkedin } from 'react-icons/tfi';
 
-function SideNavbar() {
+const SideNavbar = () => {
   return (
     <section className="sidebar">
-      <header>
-        <h2 className="fs-3">Offcanvas</h2>
-        <button type="button">&times;</button>
-      </header>
-      <div className="d-flex gap-1 flex-column">
-        <Link to="/">Home</Link>
-        <Link to="/reservation">Reservation</Link>
-        <Link to="/new_course">New Course</Link>
-        <Link to="/delete_course">Delete Course</Link>
-        <Link to="/login">Log In</Link>
+      <h2 className="sidebar__brand">ClassGo</h2>
+      <div className="sidebar__nav">
+        <NavLink className="sidebar__navlink" to="/">
+          Home
+        </NavLink>
+        <NavLink className="sidebar__navlink" to="/reservation">
+          Reservation
+        </NavLink>
+        <NavLink className="sidebar__navlink" to="/new_course">
+          New Course
+        </NavLink>
+        <NavLink className="sidebar__navlink" to="/delete_course">
+          Delete Course
+        </NavLink>
+        <NavLink className="sidebar__navlink" to="/login">
+          Log In
+        </NavLink>
       </div>
+      <footer className="sidebar__footer">
+        <div className="d-flex p-1 justify-content-around">
+          <a
+            className="sidebar__social"
+            href="https://twitter.com/Tanveer98589023"
+            target="__blank"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            className="sidebar__social"
+            href="http://indiecodermm.github.io/hthant-portfolio/"
+            target="__blank"
+          >
+            <FaFacebookF />
+          </a>
+          <a
+            className="sidebar__social"
+            href="https://linkedin.com/in/tobuya"
+            target="__blank"
+          >
+            <TfiLinkedin />
+          </a>
+          <a
+            className="sidebar__social"
+            href="https://github.com/IndieCoderMM/e-learning-academy"
+            target="__blank"
+          >
+            <TfiGithub />
+          </a>
+        </div>
+        <p>&copy; 2023 elearningacademy.com </p>
+      </footer>
     </section>
   );
-}
+};
 
 export default SideNavbar;
 
