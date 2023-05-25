9790c28a8e5d1df65b4fd6f5598353a8359548a3
tobuya <thomasobuya95@gmail.com>
2023-05-25 16:18:15 +0300

    Add a view for users who are not logged in

--- app/javascript/pages/NewCourse.js
+++ app/javascript/pages/NewCourse.js
@@ -1,11 +1,12 @@
 /* eslint-disable camelcase */
 import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import {
   Button, Container, Form, Alert,
 } from 'react-bootstrap';
 import { coursesActions } from '../store/coursesSlice';
+import NewCourseAlert from '../components/NewCourseAlert';
 
 const NewCourse = ({ createCourse }) => {
   const [title, setTitle] = useState('');
@@ -15,6 +16,7 @@ const NewCourse = ({ createCourse }) => {
   const [duration, setDuration] = useState('');
   const [img_url, setImgUrl] = useState('');
   const [message, setMessage] = useState('');
+  const currentUser = useSelector((state) => state.user);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -44,82 +46,87 @@ const NewCourse = ({ createCourse }) => {
   };
 
   return (
-    <Container className="d-flex flex-column justify-content-center align-items-center gap-2 p-3 new-course-form-container" fluid>
-      <h1>Add New Course</h1>
+    <section className="page">
+      <NewCourseAlert />
+      {currentUser.id != null ? (
+        <Container className="d-flex flex-column justify-content-center align-items-center gap-2 p-3 new-course-form-container" fluid>
+          <h1>Add New Course</h1>
 
-      <Form onSubmit={handleSubmit} className="d-flex flex-column p-3 text-dark bg-white rounded w-50 h-50">
-        {message && <Alert variant={message.includes('failed') ? 'danger' : 'success'}>{message}</Alert>}
-        <Form.Label>
-          Title:
-          <Form.Control
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
-        </Form.Label>
-        <br />
+          <Form onSubmit={handleSubmit} className="d-flex flex-column p-3 text-dark bg-white rounded w-50 h-50">
+            {message && <Alert variant={message.includes('failed') ? 'danger' : 'success'}>{message}</Alert>}
+            <Form.Label>
+              Title:
+              <Form.Control
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+            </Form.Label>
+            <br />
 
-        <Form.Label>
-          Course Image URL:
-          <Form.Control
-            type="text"
-            value={img_url}
-            onChange={(e) => setImgUrl(e.target.value)}
-            required
-          />
-        </Form.Label>
-        <br />
+            <Form.Label>
+              Course Image URL:
+              <Form.Control
+                type="text"
+                value={img_url}
+                onChange={(e) => setImgUrl(e.target.value)}
+                required
+              />
+            </Form.Label>
+            <br />
 
-        <Form.Label>
-          Instructor:
-          <Form.Control
-            type="text"
-            value={instructor}
-            onChange={(e) => setInstructor(e.target.value)}
-            required
-          />
-        </Form.Label>
-        <br />
+            <Form.Label>
+              Instructor:
+              <Form.Control
+                type="text"
+                value={instructor}
+                onChange={(e) => setInstructor(e.target.value)}
+                required
+              />
+            </Form.Label>
+            <br />
 
-        <Form.Label>
-          Description:
-          <Form.Control
-            type="textarea"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            required
-          />
-        </Form.Label>
-        <br />
+            <Form.Label>
+              Description:
+              <Form.Control
+                type="textarea"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required
+              />
+            </Form.Label>
+            <br />
 
-        <Form.Label>
-          Price:
-          <Form.Control
-            type="number"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            required
-          />
-        </Form.Label>
-        <br />
+            <Form.Label>
+              Price:
+              <Form.Control
+                type="number"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                required
+              />
+            </Form.Label>
+            <br />
 
-        <Form.Label>
-          Duration:
-          <Form.Control
-            type="number"
-            value={duration}
-            onChange={(e) => setDuration(e.target.value)}
-            required
-          />
-        </Form.Label>
-        <br />
+            <Form.Label>
+              Duration:
+              <Form.Control
+                type="number"
+                value={duration}
+                onChange={(e) => setDuration(e.target.value)}
+                required
+              />
+            </Form.Label>
+            <br />
 
-        <Button variant="primary" type="submit">
-          Create Course
-        </Button>
-      </Form>
-    </Container>
+            <Button variant="primary" type="submit">
+              Create Course
+            </Button>
+          </Form>
+        </Container>
+      ) : null}
+    </section>
   );
 };
 
