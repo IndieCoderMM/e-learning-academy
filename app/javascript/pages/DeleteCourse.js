58468b815ff8236ec1c7313f8fb73f1dfca99375
tobuya <thomasobuya95@gmail.com>
2023-05-25 16:18:59 +0300

    Add a view for users who are not logged in

--- app/javascript/pages/DeleteCourse.js
+++ app/javascript/pages/DeleteCourse.js
@@ -1,18 +1,25 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { coursesActions } from '../store/coursesSlice';
-import DeleteCourseItem from '../components/CourseItem';
+import DeleteCourseItem from '../components/DeleteCourseItem';
 import CustomCarousel from '../components/CustomCarousel';
+import DeleteCourseAlert from '../components/DeleteCourseAlert';
 
 const DeleteCourse = () => {
   const dispatch = useDispatch();
   const courses = useSelector((state) => state.courses.courses);
-  const loading = useSelector((state) => state.courses.loading);
+  const currentUser = useSelector((state) => state.user);
   let coursesItems = [];
 
   useEffect(() => {
-    dispatch(coursesActions.fetchCourses());
-  }, [dispatch]);
+    if (currentUser.id != null) {
+      if ((courses.length > 0
+          && courses[0].user_id !== currentUser.id)
+      ) {
+        dispatch(coursesActions.fetchCourses());
+      }
+    }
+  }, [dispatch, courses, currentUser.id]);
 
   if (courses.length) {
     coursesItems = [];
@@ -25,13 +32,12 @@ const DeleteCourse = () => {
     });
   }
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <section className="page">
-      <CustomCarousel items={coursesItems} />
+      <DeleteCourseAlert />
+      {coursesItems.length > 0 && currentUser.id != null ? (
+        <CustomCarousel items={coursesItems} />
+      ) : null}
     </section>
   );
 };
