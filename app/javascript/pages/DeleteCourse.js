12a1998dd659405d674c7c44df22bd278bc4809f
tobuya <thomasobuya95@gmail.com>
2023-05-25 09:25:08 +0300

    Add a delete button to courses

--- app/javascript/pages/DeleteCourse.js
+++ app/javascript/pages/DeleteCourse.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { coursesActions } from '../store/coursesSlice';
+import DeleteCourseItem from '../components/CourseItem';
+import CustomCarousel from '../components/CustomCarousel';
 
-function DeleteCourse() {
+const DeleteCourse = () => {
+  const dispatch = useDispatch();
+  const courses = useSelector((state) => state.courses.courses);
+  const loading = useSelector((state) => state.courses.loading);
+  let coursesItems = [];
+
+  useEffect(() => {
+    dispatch(coursesActions.fetchCourses());
+  }, [dispatch]);
+
+  if (courses.length) {
+    coursesItems = [];
+    courses.forEach((course) => {
+      const item = {
+        element: <DeleteCourseItem course={course} />,
+        key: course.id,
+      };
+      coursesItems.push(item);
+    });
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>DeleteCourse</div>
+    <section className="page">
+      <CustomCarousel items={coursesItems} />
+    </section>
   );
-}
+};
 
 export default DeleteCourse;
