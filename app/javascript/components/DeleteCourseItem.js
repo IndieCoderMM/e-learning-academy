89250ab708ac707364ddc355e2314f74a7a5474d
tobuya <thomasobuya95@gmail.com>
2023-05-26 12:16:49 +0300

    Add a method that handles delete and dispatch an action to fetch new courses data

--- app/javascript/components/DeleteCourseItem.js
+++ app/javascript/components/DeleteCourseItem.js
@@ -1,58 +1,69 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
 import {
-  FaGraduationCap, FaClock, FaMoneyBillAlt, FaTrash,
+  FaGraduationCap, FaClock, FaMoneyBillAlt,
 } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+import { coursesActions } from '../store/coursesSlice';
 
-const DeleteCourseItem = ({ course, onDelete }) => (
-  <div key={course.id} className="course-card">
-    <div className="course-card__frame">
-      <img
-        src={course.img_url}
-        alt={course.title}
-        className="course-card__img"
-      />
-    </div>
-    <Link to={`/courses/${course.id}`} className="course-card__title">
-      {course.title}
-    </Link>
-    <hr className="course-card__hr" />
+const DeleteCourseItem = ({ course }) => {
+  const dispatch = useDispatch();
 
-    <p>{course.description}</p>
-    <div className="course-card__details">
-      <div>
-        <span className="course-card__icon">
-          <FaMoneyBillAlt />
-          {' '}
-        </span>
-        <p>
-          $
-          {course.price}
-        </p>
+  const handleDelete = () => {
+    dispatch(coursesActions.destroyCourse(course.id))
+      .then(() => dispatch(coursesActions.fetchCourses()));
+  };
+
+  return (
+    <div key={course.id} className="course-card">
+      <div className="course-card__frame">
+        <img src={course.img_url} alt={course.title} className="course-card__img" />
       </div>
-      <div>
-        <span className="course-card__icon">
-          <FaClock />
-        </span>
-        <p>
-          {course.duration}
-          {' '}
-          minutes
-        </p>
+      <Link to={`/courses/${course.id}`} className="course-card__title">
+        {course.title}
+      </Link>
+      <hr className="course-card__hr" />
+      <p>{course.description}</p>
+      <div className="course-card__details">
+        <div>
+          <span className="course-card__icon">
+            <FaMoneyBillAlt />
+          </span>
+          <p>
+            $
+            {course.price}
+          </p>
+        </div>
+        <div>
+          <span className="course-card__icon">
+            <FaClock />
+          </span>
+          <p>
+            {course.duration}
+            {' '}
+            minutes
+          </p>
+        </div>
+        <div>
+          <span className="course-card__icon">
+            <FaGraduationCap />
+          </span>
+          <p>{course.instructor}</p>
+        </div>
       </div>
-      <div>
-        <span className="course-card__icon">
-          <FaGraduationCap />
-        </span>
-        <p>{course.instructor}</p>
-      </div>
+      <Button
+        type="button"
+        className="course-card__action w-100 d-flex align-items-center justify-content-center fw-bold"
+        variant="outline-danger"
+        onClick={handleDelete}
+      >
+        Delete Course
+      </Button>
     </div>
-    <button type="button" className="course-card__icon course-card_delete" onClick={() => onDelete(course.id)}>
-      <FaTrash />
-    </button>
-  </div>
-);
+  );
+};
 
 DeleteCourseItem.propTypes = {
   course: PropTypes.shape({
@@ -64,7 +75,6 @@ DeleteCourseItem.propTypes = {
     price: PropTypes.number.isRequired,
     duration: PropTypes.number.isRequired,
   }).isRequired,
-  onDelete: PropTypes.func.isRequired,
 };
 
 export default DeleteCourseItem;
